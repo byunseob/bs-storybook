654cf3adcde8010e5b1d102d5a573fede6f575f2
“byunseob” <grayhulk412@gmail.com>
2020-03-01 08:04:03 +0900

    Init byunseob design system
    Add Button by https://velog.io/@velopert/create-your-own-design-system-with-storybook

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+    stories: ['../src/**/*.stories.js','../src/**/*.stories.mdx','../src/**/*.stories.tsx'],
+    addons: ['@storybook/addon-actions',
+        '@storybook/addon-links',
+        '@storybook/addon-knobs/register',
+        '@storybook/addon-docs/react/preset'],
+};
